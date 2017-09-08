8bf864dfaaabfe72c1ce2da7c7f7bc34db5c33d3
lymanjohnson <lyman.johnson@gmail.com>
2017-09-08 13:30:39 -0400

    Added listener, fixed urls from / to /api/

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const fs = require('fs'),
   path = require('path'),
   express = require('express'),
-  mustacheExpress = require('mustache-express'),
+  // mustacheExpress = require('mustache-express'),
   session = require('express-session'),
   bodyParser = require('body-parser'),
   // flash = require('express-flash-messages'),
@@ -43,38 +43,42 @@ app.use(session({
 }));
 // <-- Not sure I need what is in between these lines //
 
-app.get('/activities/', function(req,res) {
+app.get('/api/activities/', function(req,res) {
 // Show a list of all activities I am tracking, and links to their individual pages
-
+  console.log("get activities");
 })
 
-app.post('/activities/', function(req,res) {
+app.post('/api/activities/', function(req,res) {
 // Create a new activity for me to track.
-
+  console.log("post activities");
 })
 
-app.get('/activities/:id', function(req,res) {
+app.get('/api/activities/:id', function(req,res) {
 // Show information about one activity I am tracking, and give me the data I have recorded for that activity.
-
+  console.log("get activities id");
 })
 
-app.put('/activities/:id', function(req,res) {
+app.put('/api/activities/:id', function(req,res) {
 // Update one activity I am tracking, changing attributes such as name or type. Does not allow for changing tracked data.
-
+  console.log("put activities id");
 })
 
 app.delete('activities/:id', function(req,res) {
 // Delete one activity I am tracking. This should remove tracked data for that activity as well.
-
+  console.log("delete activities id");
 })
 
 
-app.post('/activities/:id/stats', function(req,res) {
+app.post('/api/activities/:id/stats', function(req,res) {
 // Add tracked data for a day. The data sent with this should include the day tracked. You can also override the data for a day already recorded.
 
 })
 
-app.delete('/stats/:id', function(req,res) {
+app.delete('/api/stats/:id', function(req,res) {
 //	Remove tracked data for a day.
 
 })
+
+app.listen(3000, function() {
+  console.log('Express running on http://localhost:3000/.')
+});
